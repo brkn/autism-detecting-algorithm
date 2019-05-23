8d82e5d8c7a5d8657c61f79e6d01daaea59e4b4d
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-23 23:29:53 +0300

    Return rounded prediction array

--- predict_data.py
+++ predict_data.py
@@ -1,4 +1,10 @@
 def predict_data(model, test_x):  # model&Xtst
     predictions = model.predict(test_x, batch_size=None, verbose=0, steps=None)
-    return predictions
+    predictions_rounded = []
+    for prediction in predictions:
+        if prediction >= 0.5:
+            predictions_rounded.append(1)
+        else:
+            predictions_rounded.append(0)
+    return predictions_rounded
 

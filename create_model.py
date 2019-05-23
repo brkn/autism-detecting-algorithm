2365ffad9ce6f9fab4fa97dc4ae3ecb063240019
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-23 22:09:35 +0300

    Create create_model function.

--- create_model.py
+++ create_model.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import keras
+from keras.models import Sequential
+from keras.layers import Dense
 
-def create_model():
-    pass
+def create_model(input_shape):
+    model = Sequential()
+    model.add(Dense(4, input_dim=input_shape, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))

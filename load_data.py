fa70fcaf1d6788fbb864f999f1711502a3685a71
Berkan Unal <Berkanunal@gmail.com>
2019-05-23 21:56:53 +0300

    Fix data paths at load_data function

--- load_data.py
+++ load_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os
 
 def load_data(): # Xpaths->Xtra&Xtst
-    test_df = pd.read_csv("data/test.csv")
-    train_df = pd.read_csv("data/training.csv")
+    current_working_dir = os.getcwd()
+    test_path = f"{current_working_dir}/data/test.csv"
+    train_path = f"{current_working_dir}/data/train.csv"
+
+    test_df = pd.read_csv(test_path)
+    train_df = pd.read_csv(train_path)
     return test_df, train_df
 

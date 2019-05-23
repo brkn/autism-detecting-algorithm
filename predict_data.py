52be042cd25dc27718d0cf4c72558c3931436011
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-23 23:23:25 +0300

    Return predictions

--- predict_data.py
+++ predict_data.py
@@ -1,3 +1,4 @@
 def predict_data(model, test_x):  # model&Xtst
-    model.predict(test_x, batch_size=None, verbose=0, steps=None)
+    predictions = model.predict(test_x, batch_size=None, verbose=0, steps=None)
+    return predictions
 

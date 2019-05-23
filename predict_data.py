e5734d52c23d7521767d97215a990229a8d35878
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-23 22:57:35 +0300

    Delete import and change parameters of predict function

--- predict_data.py
+++ predict_data.py
@@ -1,5 +1,3 @@
-import keras
-
 def predict_data(model, test_x):  # model&Xtst
-    model.predict(test_x, batch_size=None, verbose=0, steps=None, callbacks=None)
+    model.predict(test_x, batch_size=None, verbose=0, steps=None)
 

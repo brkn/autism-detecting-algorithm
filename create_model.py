79dbcfd4af17ab1741c2b71519802f6cbaf970f6
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-24 00:29:04 +0300

    Change model

--- create_model.py
+++ create_model.py
@@ -5,12 +5,7 @@ from keras.layers import Dense, Dropout
 
 def create_model(input_shape):
     model = Sequential()
-    model.add(Dense(8, input_dim=input_shape, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dropout(0.2))
+    model.add(Dense(100, input_dim=input_shape, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 

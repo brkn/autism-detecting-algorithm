48f4110ac02272bf46bbf92b25a612af28c7eecf
Berkan Unal <Berkanunal@gmail.com>
2019-05-23 23:20:09 +0300

    Add get_latest_filename to write_output

--- write_output.py
+++ write_output.py
@@ -1,2 +1,25 @@
-def write_output(result):  #->submission.csv
-    pass
+import csv
+import os
+
+
+def write_output(result):  # ->submission.csv
+    latest_file_directory = get_latest_filename()
+
+    with open(latest_file_directory, 'w') as write_file:
+        writer = csv.writer(write_file)
+        writer.writerows("lines")
+
+
+def get_latest_filename():
+    current_working_dir = os.getcwd()
+    submissions_directory = f"{current_working_dir}/submissions"
+
+    files_array = []
+    for _, _, files in os.walk(submissions_directory):
+        if len(files) == 0:
+            return f"{current_working_dir}/submissions/submission_001.csv"
+        for file in files:
+            if "submission" in file:
+                files_array.extend(file.replace(".csv", ""))
+
+    return f"{current_working_dir}/{files_array[-1]}.csv"

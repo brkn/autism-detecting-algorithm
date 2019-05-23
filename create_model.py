5e61e0694c5995850ed80a20a55ad9fb422c2b6c
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-23 22:54:55 +0300

    Fix errors and  change optimizer

--- create_model.py
+++ create_model.py
@@ -5,11 +5,11 @@ from keras.layers import Dense
 
 def create_model(input_shape):
     model = Sequential()
-    model.add(Dense(4, input_dim=input_shape, activation='relu'))
+    model.add(Dense(4, input_shape=input_shape, activation='relu'))
     model.add(Dense(8, activation='relu'))
     model.add(Dense(8, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy', optimizer='opt', metrics=['accuracy','loss']) 
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
     # optimizer may be adam
     return model

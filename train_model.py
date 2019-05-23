888a78dfa19ff323f17b7b0f41541261b62cd83b
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-05-23 22:15:55 +0300

    Create train_model function.

--- train_model.py
+++ train_model.py
@@ -1,2 +1,2 @@
-def train_model(train_df):   # Xtra || Xtra^new->model
-    pass
+def train_model(model, X_train, y_train):   # Xtra || Xtra^new->model
+    model.fit(X_train, y_train, epochs=50, verbose=0)

a9b2535cdeffeade1f5bc4df7fff9057e9fe510c
Berkan Unal <Berkanunal@gmail.com>
2019-05-23 22:07:43 +0300

    Add remove_columns_with_single_unique_value for preprocessing

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,12 +1,25 @@
+import pandas as pd
+
 MAX_VALUE = 1
 MIN_VALUE = 0
 
+
 def preprocess_data(data):    # Xtra->Xtra^new
     data = remove_noise(data)
+    data = remove_columns_with_single_unique_value(data)
     return data
+
 
 def remove_noise(data):
     for column_name in data:
         if data[column_name].max() > MAX_VALUE or data[column_name].min() < MIN_VALUE:
-            data = data.drop(data[data[column_name] < MIN_VALUE or data[column_name] > MAX_VALUE].index)
+            data = data.drop(
+                data[data[column_name] < MIN_VALUE or data[column_name] > MAX_VALUE].index)
+    return data
+
+
+def remove_columns_with_single_unique_value(data):
+    nunique_datas = data.apply(pd.Series.nunique)
+    cols_to_drop = nunique_datas[nunique_datas == 1].index
+    data.drop(cols_to_drop, axis=1)
     return data

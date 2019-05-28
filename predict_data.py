88778ee76d55911e8c43f414b8e0bb626d6a10df
Berkan Unal <Berkanunal@gmail.com>
2019-05-28 23:55:02 +0300

    Add model_type checking logic to predict_data function

--- predict_data.py
+++ predict_data.py
@@ -1,9 +1,18 @@
-def predict_data(model, test_x):  # model&Xtst
-    predictions = model.predict(test_x, batch_size=None, verbose=0, steps=None)
-    predictions_rounded = []
-    for prediction in predictions:
-        if prediction >= 0.5:
-            predictions_rounded.append(1)
-        else:
-            predictions_rounded.append(0)
+from train_model import MODEL_TYPES
+
+
+def predict_data(model, model_type, test_x):  # model&Xtst
+    if model_type == MODEL_TYPES["NEURAL_NETWORK"]:
+        predictions = model.predict(
+            test_x, batch_size=None, verbose=0, steps=None)
+        predictions_rounded = []
+        for prediction in predictions:
+            if prediction >= 0.5:
+                predictions_rounded.append(1)
+            else:
+                predictions_rounded.append(0)
+
+    elif model_type == MODEL_TYPES["SUPPORT_VECTOR_MACHINE"]:
+        predictions_rounded = model.predict(test_x)
+
     return predictions_rounded
